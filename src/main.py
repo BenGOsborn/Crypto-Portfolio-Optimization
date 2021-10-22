8e432cbce0fec083e2850b489b80acddca55d167
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-10-23 08:36:39 +1100

    🎉 Update

--- src/main.py
+++ src/main.py
@@ -7,8 +7,6 @@ except:
     pass
 
 from utils import DataClass, Utils
-import itertools
-import numpy as np
 
 
 def main():
@@ -22,21 +20,6 @@ def main():
     # https://youtu.be/vHzlZECzyPE - Correlation
 
     # Get the data pairs
-    pairs = data.get_pairs()
-    np.random.shuffle(pairs)
-
-    correlations = []
-    days = 20
-    for pair in itertools.combinations(pairs[:10], 2):
-        try:
-            data1 = data.get_data(pair[0], days)
-            data2 = data.get_data(pair[1], days)
-            corr = Utils.correlation(data1, data2)
-            correlations.append((pair[0], pair[1], corr))
-        except Exception as e:
-            print(f"Encountered exception '{e}' for pair '{pair}'")
-
-    sorted_correlations = sorted(correlations, key=lambda x: x[2])
     print(sorted_correlations)
 
 
--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ from binance import Client
 import pandas as pd
 import numpy as np
 from datetime import datetime
+import itertools
 
 
 class DataClass:
@@ -40,9 +41,31 @@ class Utils:
     # Return the price percent changes correlation coefficient
     @staticmethod
     def correlation(df1: pd.DateOffset, df2: pd.DataFrame):
-        close1 = df1["close"].pct_change().rolling(12).mean()
-        close2 = df2["close"].pct_change().rolling(12).mean()
+        close1 = df1["close"].pct_change().rolling(12).mean().dropna()
+        close2 = df2["close"].pct_change().rolling(12).mean().dropna()
         corr = np.corrcoef(close1.values[1:], close2.values[1:])[0, 1]
         if corr == np.nan:
             raise Exception("Returned NaN")
         return corr
+
+    # Rank token pairs based on the most correlated or inversely correlated
+    @staticmethod
+    def get_correlations(data: DataClass, days: int, limit: int = None, inverse: bool = True):
+        pairs = data.get_pairs()
+        if limit != None:
+            pairs = pairs[:limit]
+            np.random.shuffle(pairs)
+
+        correlations = []
+        for pair in itertools.combinations(pairs, 2):
+            try:
+                data1 = data.get_data(pair[0], days)
+                data2 = data.get_data(pair[1], days)
+                corr = Utils.correlation(data1, data2)
+                correlations.append((pair[0], pair[1], corr))
+            except Exception as e:
+                print(f"Encountered exception '{e}' for pair '{pair}'")
+
+        sorted_correlations = sorted(
+            correlations, key=lambda x: x[2], reverse=not inverse)
+        return sorted_correlations
