041b47f6c87e4815283ab9af5f73217f033f4a99
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-10-22 22:08:47 +1100

    🎉 Update

--- src/main.py
+++ src/main.py
@@ -9,9 +9,15 @@ except:
 
 
 def main():
+    # Initialize the API
     api_key = os.getenv("API_KEY")
     api_secret = os.getenv("API_SECRET")
-    x = Client(api_key)
+    client = Client(api_key)
+
+    data = client.get_historical_klines(
+        "ETHUSDT", client.KLINE_INTERVAL_1HOUR, "1 day ago UTC")
+
+    print(len(data))
 
 
 if __name__ == "__main__":
