51a04eac9344464209680116e280aedac81f0643
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-11-06 14:11:00 +1100

    🎉 Update

--- src/main.py
+++ src/main.py
@@ -21,7 +21,7 @@ def main():
     # https://youtu.be/vHzlZECzyPE - Correlation
 
     pairs = [x + "USDT" for x in ["BTC", "ETH", "BNB",
-                                  # "SOL", "FTM", "LINK", "AVAX", "GRT", "RUNE", "ALGO", "BUSD"
+                                  "SOL", "FTM", "LINK", "AVAX", "GRT", "RUNE", "ALGO", "BUSD"
                                   ]]
     cache = {pair: data_class.get_data(pair, 30) for pair in pairs}
 
