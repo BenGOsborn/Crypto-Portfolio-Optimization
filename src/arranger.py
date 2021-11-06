9f957e3e24552e99e5ec703c97fcc4e36eee90d4
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-11-06 14:34:06 +1100

    🎉 Update

---
+++ src/arranger.py
@@ -0,0 +1,6 @@
+# Rearranges the users portfolio to the new assets based off of their old portfolio
+
+# Steps
+# 1. Get the previous percentages and compare them with the new ones
+# 2. Seperate the ones that are decreasing in allocation vs the ones that are gaining in allocation
+# 3. Remove the specified amount from the ones that are decreasing and then use this to fill up the ones that are increasing bit by bit
